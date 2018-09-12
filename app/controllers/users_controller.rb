e1955db0f56ca669ebb551a8a8b20b6f13dbf03c
Clément Hampaï <clement.hampai@cypressxt.net>
2018-09-12 10:19:54 +0200

    Add Update user function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :must_be_proprietary, only:   [:edit, :update, :show]
+
 
   def new
     @user = User.new
@@ -17,6 +19,34 @@ class UsersController < ApplicationController
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    if user_params.has_key?(:password) && user_params.has_key?(:password_confirmation)
+      @user.password = user_params["password"]
+      @user.password_confirmation = user_params["password_confirmation"]
+      if @user.password == @user.password_confirmation
+        @user.gen_token_and_salt
+        @user.change_password(@user.password)
+      end
+    end
+    if @user.update(user_params.except(:password, :password_confirmation))
+      flash[:success] = "Profil successfully updated."
+      redirect_to user_path(@user.id)
+    else
+      flash[:danger] =  "Failed to update "+@user.firstname + ", "+@user.errors.full_messages.to_sentence+"."
+      redirect_to edit_user_path(@user.id)
+    end
+  end
+
+  def must_be_proprietary
+    @user = User.find_by(id: params[:user_id]) || User.find_by(id: params[:id])
+    if @user != current_logged_user
+      render_403
+    end
   end
 
   def user_params
